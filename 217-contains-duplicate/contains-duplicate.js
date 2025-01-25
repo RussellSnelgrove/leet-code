ac52092ed3cd0bbb9a76ee68f179833ea40aa272
Russell Snelgrove <22Russell22@gmail.com>
2025-01-24 19:11:16 -0700

    Time: 12 ms (70.69%) | Memory: 59.8 MB (89.39%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -4,12 +4,12 @@
  */
  
 var containsDuplicate = function(nums) {
-    return new Set(nums).size < nums.length;
+    // return new Set(nums).size < nums.length;
     // save each element to a list
-    // const hashSet = new Set();
-    // for (let i = 0; i < nums.length; i++ ){
-    //     if (hashSet.has(nums[i])) return true
-    //     hashSet.add(nums[i]);
-    // }
-    // return false;
+    const hashSet = new Set();
+    for (let i = 0; i < nums.length; i++ ){
+        if (hashSet.has(nums[i])) return true
+        hashSet.add(nums[i]);
+    }
+    return false;
 };
