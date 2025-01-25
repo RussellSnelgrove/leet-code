425870a98b51fee7b41cd618c39e4ab602f8a65a
Russell Snelgrove <22Russell22@gmail.com>
2025-01-24 18:53:56 -0700

    Time: 3 ms (68.36%) | Memory: 52 MB (15.27%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 
-
-var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length-1; i++) {
-        for (let j = i+1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) return [i, j];
-        }
+var twoSum = function (nums, target) {
+    const numsHashMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+        if (numsHashMap.has(diff)) return [numsHashMap.get(diff), i]
+        numsHashMap.set(nums[i], i);
     }
 };
