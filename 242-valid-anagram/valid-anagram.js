7a1ebeec282b9c2c8f8a815a902e4444dfa9e5b4
Russell Snelgrove <22Russell22@gmail.com>
2025-01-24 19:40:55 -0700

    Time: 10 ms (87.85%) | Memory: 50.7 MB (91.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,8 +4,23 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false
-    const stringS = s.split('').sort().join('');
-    const stringT = t.split('').sort().join('');
-    return stringS === stringT;
+    if (s.length !== t.length) return false;
+    const sMap = {};
+    const tMap = {};
+
+    for (let i = 0; i < s.length; i++){
+        sMap[s[i]] = 1 + (sMap[s[i]] || 0 )
+        tMap[t[i]] = 1 + (tMap[t[i]] || 0 )
+    }
+
+    for (const key in sMap) {
+        if(sMap[key] !== tMap[key]) return false;
+    }
+    return true
+
+    // sorting uses less memory but sorts lists twice
+    // if (s.length !== t.length) return false;
+    // const stringS = s.split('').sort().join('');
+    // const stringT = t.split('').sort().join('');
+    // return stringS === stringT;
 };
