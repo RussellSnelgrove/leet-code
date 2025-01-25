50e7530ed5600e9f2d8ab115401666eebcc4c6d7
Russell Snelgrove <22Russell22@gmail.com>
2025-01-25 16:23:20 -0700

    Time: 15 ms (54.71%) | Memory: 53.7 MB (66.88%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,35 +4,30 @@
  * @return {number[]}
  */
 var topKFrequent = function (nums, k) {
-    const res = [];
-    const hashNumFrequency = {};
-    for (const num in nums){
-        hashNumFrequency[nums[num]] = (hashNumFrequency[nums[num]] || 0) + 1;
-    }
-    const bucketOfFrequency = Array.from({ length: nums.length + 1 }, () => []);
-
-    for ( const num in hashNumFrequency){
-      bucketOfFrequency[hashNumFrequency[num]].push(parseInt(num));
-    }
-    for(let i = bucketOfFrequency.length-1; i > 0; i--){
-      for (const n of bucketOfFrequency[i]){
-        res.push(n);
-        if(res.length ===k) return res;
-      }
-    }
-
-    // const result = {};
+    // const res = [];
+    // const hashNumFrequency = {};
     // for (const num in nums){
-    //     result[num] = (result[num] || 0) +1;
+    //     hashNumFrequency[nums[num]] = (hashNumFrequency[nums[num]] || 0) + 1;
     // }
-    // for (let i = 0; i < nums.length; i++) {
-    //     if (result[nums[i]] === undefined) result[nums[i]] = 0;
-    //     result[nums[i]]++;
+    // const bucketOfFrequency = Array.from({ length: nums.length + 1 }, () => []);
+
+    // for ( const num in hashNumFrequency){
+    //   bucketOfFrequency[hashNumFrequency[num]].push(parseInt(num));
+    // }
+    // for(let i = bucketOfFrequency.length-1; i > 0; i--){
+    //   for (const n of bucketOfFrequency[i]){
+    //     res.push(n);
+    //     if(res.length ===k) return res;
+    //   }
     // }
 
-    // const sortedObject = Object.fromEntries(
-    //     Object.entries(unsortedObject).sort(([, a], [, b]) => b - a)
-    // );
-
-    //     return Object.values(result);
+    const result = {};
+  
+    for (let i = 0; i < nums.length; i++) {
+        if (result[nums[i]] === undefined) result[nums[i]] = 0;
+        result[nums[i]]++;
+    }
+    const arr = Object.entries(result).map(([num, freq]) => [freq, parseInt(num)]);
+    arr.sort((element1, element2) => element2[0] - element1[0]);
+    return arr.slice(0,k).map(element => element[1])
 };
