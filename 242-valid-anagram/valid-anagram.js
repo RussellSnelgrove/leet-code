ccfe9fd06564e0e200fea1153808bf6f16bd915a
Russell Snelgrove <22Russell22@gmail.com>
2025-01-24 19:18:28 -0700

    Time: 29 ms (26.67%) | Memory: 53.3 MB (50.34%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,6 +4,7 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false
     const stringS = s.split('').sort().join('');
     const stringT = t.split('').sort().join('');
     return stringS === stringT;
