4a037881461dcff9d7b0f5de1c63274b023561ba
Russell Snelgrove <22Russell22@gmail.com>
2025-01-27 15:39:45 -0700

    Time: 1 ms (91.60%) | Memory: 49.4 MB (96.16%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -4,11 +4,26 @@
  * @return {number[]}
  */
 var twoSum = function (numbers, target) {
-    const numberSet = new Map()
-    for(let i = 0; i < numbers.length; i++ ){
-        if (numberSet.has(target - numbers[i])){
-            return [numberSet.get(target - numbers[i]), i+1];
+    let left = 0
+    let right = numbers.length - 1;
+    while (left < right) {
+        const currentValue = numbers[left] + numbers[right];
+        if (currentValue > target) {
+            right--;
+        } else if (target > currentValue) {
+            left++;
+        } else {
+            return [left+1, right+1];
         }
-        numberSet.set(numbers[i], i + 1);
     }
+
+    // Using Hash Map
+    // Problem is that the space used is O(n) pointers will be O(1)
+    // const numberSet = new Map()
+    // for(let i = 0; i < numbers.length; i++ ){
+    //     if (numberSet.has(target - numbers[i])){
+    //         return [numberSet.get(target - numbers[i]), i+1];
+    //     }
+    //     numberSet.set(numbers[i], i + 1);
+    // }
 };
