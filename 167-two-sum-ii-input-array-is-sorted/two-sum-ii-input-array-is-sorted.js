d0a1c3c67eb56a70d12c9d0b0e59ac4ee7d043c3
Russell Snelgrove <22Russell22@gmail.com>
2025-01-27 15:30:35 -0700

    Time: 4 ms (19.17%) | Memory: 50.3 MB (43.55%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function (numbers, target) {
-    const numberSet = new Set(numbers);
+    const numberSet = new Map()
     for(let i = 0; i < numbers.length; i++ ){
         if (numberSet.has(target - numbers[i])){
-            const index2 = numbers.lastIndexOf(target - numbers[i]);
-            if (index2 !== i) return [i+1, index2+1];
+            return [numberSet.get(target - numbers[i]), i+1];
         }
+        numberSet.set(numbers[i], i + 1);
     }
 };
