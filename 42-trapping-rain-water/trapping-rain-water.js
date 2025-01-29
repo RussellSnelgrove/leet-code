416b6a2c257030179d6e1fa2ab5669a328927763
Russell Snelgrove <22Russell22@gmail.com>
2025-01-28 22:18:55 -0700

    Time: 0 ms (100.00%) | Memory: 51.2 MB (90.56%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -2,6 +2,29 @@
  * @param {number[]} height
  * @return {number}
  */
+// using Arrays but the arrays use O(n) memory
+// var trap = function (height) {
+//     const maxLeftArray = [];
+//     const maxRightArray = [];
+//     let maxLeft = 0;
+//     let maxRight = 0;
+//     let waterTrapped = 0;
+//     for (let i = 0; i < height.length; i++) {
+//         maxLeftArray[i] = maxLeft;
+//         if (height[i] > maxLeft) maxLeft = height[i];
+//     }
+//     for (let i = height.length - 1; i >= 0; i--) {
+//         maxRightArray[i] = maxRight;
+//         if (height[i] > maxRight) maxRight = height[i];
+//     }
+//     for (let i = 0; i < height.length; i++) {
+//         const value = Math.min(maxLeftArray[i], maxRightArray[i]) - height[i];
+//         if (value > 0 ) waterTrapped += value;
+//     }
+//     return waterTrapped;
+// };
+
+// using pointers using O(1) memory
 var trap = function (height) {
     let rightPointer = height.length - 1;
     let leftPointer = 0;
