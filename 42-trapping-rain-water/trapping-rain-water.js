e546c80ed881842e10d1e4e7ee5db96aba89f887
Russell Snelgrove <22Russell22@gmail.com>
2025-01-28 22:17:35 -0700

    Time: 0 ms (100.00%) | Memory: 50.9 MB (95.28%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.js
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -3,22 +3,23 @@
  * @return {number}
  */
 var trap = function (height) {
-    const maxLeftArray = [];
-    const maxRightArray = [];
-    let maxLeft = 0;
-    let maxRight = 0;
-    let waterTrapped = 0;
-    for (let i = 0; i < height.length; i++) {
-        maxLeftArray[i] = maxLeft;
-        if (height[i] > maxLeft) maxLeft = height[i];
+    let rightPointer = height.length - 1;
+    let leftPointer = 0;
+    let maxRight = height[rightPointer];
+    let maxLeft = height[leftPointer];
+    let totalWater = 0;
+
+    while (leftPointer < rightPointer) {
+        if (height[leftPointer] > maxLeft) maxLeft = height[leftPointer];
+        if (height[rightPointer] > maxRight) maxRight = height[rightPointer];
+    
+        if (height[leftPointer] > height[rightPointer]) {
+            if (height[rightPointer] < maxRight) totalWater += maxRight - height[rightPointer];
+            rightPointer--;
+        } else {
+            if (height[leftPointer] < maxLeft) totalWater += maxLeft - height[leftPointer];
+            leftPointer++;
+        }
     }
-    for (let i = height.length - 1; i >= 0; i--) {
-        maxRightArray[i] = maxRight;
-        if (height[i] > maxRight) maxRight = height[i];
-    }
-    for (let i = 0; i < height.length; i++) {
-        const value = Math.min(maxLeftArray[i], maxRightArray[i]) - height[i];
-        if (value > 0 ) waterTrapped += value;
-    }
-    return waterTrapped;
+    return totalWater;
 };
