2b0079a51d55cc01d2a4e1b68b635257d50687cb
Russell Snelgrove <22Russell22@gmail.com>
2025-01-29 22:22:09 -0700

    Time: 1 ms (89.81%) | Memory: 50.8 MB (82.51%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const stack = [];
+    const closeBrackets = [')', ']', '}'];
+    for (let i = 0; i < s.length; i++) {
+        if (closeBrackets.includes(s[i])) {
+            const poppedElement = stack.pop();
+            switch (s[i]) {
+                case ')':
+                    if (poppedElement === '(') continue;
+                    return false;
+                case '}':
+                    if (poppedElement === '{') continue;
+                    return false;
+                case ']':
+                    if (poppedElement === '[') continue;
+                    return false;
+            }
+        } else {
+            stack.push(s[i]);
+        }
+    }
+    if(stack.length !== 0) return false;
+    return true;
+};
