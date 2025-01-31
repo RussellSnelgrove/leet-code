0747ef4e363978a19a8099206e517f139453bc3a
Russell Snelgrove <22Russell22@gmail.com>
2025-01-31 16:43:24 -0700

    Time: 41 ms (35.05%) | Memory: 74.2 MB (35.94%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -7,20 +7,14 @@ var dailyTemperatures = function (temperatures) {
     const result = new Array(temperatures.length);
     for (let i = 0; i < temperatures.length; i++) {
         if (stackOfTemps.length > 0) {
-            if (temperatures[i] <= stackOfTemps.at(-1).temp) {
-                stackOfTemps.push({temp: temperatures[i], index: i});
-                continue;
-            }
-            while(temperatures[i] > stackOfTemps?.at(-1)?.temp){
+            while (temperatures[i] > stackOfTemps?.at(-1)?.temp) {
                 const temperature = stackOfTemps.pop();
-                result[temperature.index] = i-temperature.index;
+                result[temperature.index] = i - temperature.index;
             }
-            stackOfTemps.push({temp: temperatures[i], index: i});
-        } else {
-            stackOfTemps.push({temp: temperatures[i], index: i});
         }
+        stackOfTemps.push({ temp: temperatures[i], index: i });
     }
-    while(stackOfTemps.length>0){
+    while (stackOfTemps.length > 0) {
         const temperature = stackOfTemps.pop();
         result[temperature.index] = 0;
     }
