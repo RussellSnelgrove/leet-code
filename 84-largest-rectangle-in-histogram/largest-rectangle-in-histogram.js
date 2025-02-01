9eda74a9a470e663173ca0d18c8ef678029753f3
Russell Snelgrove <22Russell22@gmail.com>
2025-02-01 15:48:40 -0700

    Time: 37 ms (34.73%) | Memory: 75.7 MB (20.72%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.js
@@ -0,0 +1,23 @@
+var largestRectangleArea = function (heights) {
+	let maxArea = 0;
+	let indexHieghtArray = [];
+	for (let i = 0; i < heights.length; i++) {
+		const height = heights[i];
+		let index = i;
+		if (indexHieghtArray.length === 0) {
+			indexHieghtArray.push({ index, height })
+			continue;
+		}
+		while (indexHieghtArray.length > 0 && indexHieghtArray.at(-1).height > height){
+			const element = indexHieghtArray.pop();
+			index = element.index;
+			maxArea = Math.max((i-element.index) * element.height, maxArea);
+		}
+		indexHieghtArray.push({ index, height })
+	}
+	while (indexHieghtArray.length > 0){
+		const element = indexHieghtArray.pop();
+		maxArea = Math.max((heights.length-element.index) * element.height, maxArea);
+	}
+	return maxArea;
+};
