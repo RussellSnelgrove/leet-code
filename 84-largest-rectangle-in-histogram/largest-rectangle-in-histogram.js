fddcf50c6ed41af0babb75b3b927cabe9d9b6346
Russell Snelgrove <22Russell22@gmail.com>
2025-02-01 15:52:22 -0700

    Time: 34 ms (36.87%) | Memory: 72.7 MB (25.44%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.js
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.js
@@ -1,19 +1,27 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
 var largestRectangleArea = function (heights) {
+	const heightsLength = heights.length;
 	let maxArea = 0;
 	let indexHieghtArray = [];
-	for (let i = 0; i < heights.length; i++) {
-		const height = heights[i];
+	for (let i = 0; i < heightsLength; i++) {
 		let index = i;
-		while (indexHieghtArray.length > 0 && indexHieghtArray.at(-1).height > height){
+		while (indexHieghtArray.length > 0 && indexHieghtArray.at(-1).height > heights[i]){
 			const element = indexHieghtArray.pop();
 			index = element.index;
 			maxArea = Math.max((i-element.index) * element.height, maxArea);
 		}
-		indexHieghtArray.push({ index, height })
+		indexHieghtArray.push({ index, height: heights[i] })
 	}
 	while (indexHieghtArray.length > 0){
 		const element = indexHieghtArray.pop();
-		maxArea = Math.max((heights.length-element.index) * element.height, maxArea);
+		maxArea = Math.max((heightsLength-element.index) * element.height, maxArea);
 	}
 	return maxArea;
 };
