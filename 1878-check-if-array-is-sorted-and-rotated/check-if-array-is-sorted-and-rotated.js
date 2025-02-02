129d63c3e848c3297dbbcfb00cd2a13463cbc6e2
Russell Snelgrove <22Russell22@gmail.com>
2025-02-02 16:01:59 -0700

    Time: 0 ms (100.00%) | Memory: 49.1 MB (47.15%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -4,12 +4,13 @@
  */
 var check = function (nums) {
     let maxRotations = nums.length;
-    const compareString = nums.join('-');
-    nums.sort((a,b)=>{return a-b});
-    for (let i = 0; i < maxRotations; i++) {
-        const element = nums.pop();
-        nums.unshift(element);
-        if (nums.join('-') === compareString) return true;
+    let countInARow = 0;
+    if (nums.length <=1) return true;
+    for (let i = 1; i < 2 * nums.length; i++) {
+        if (nums[i % maxRotations] >= nums[(i - 1) % maxRotations]) countInARow++;
+        if (nums[i % maxRotations] < nums[(i - 1) % maxRotations]) countInARow = 0;
+        if (countInARow === maxRotations-1) return true;
     }
     return false
 };
+
