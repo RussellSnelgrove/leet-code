60f816597b6d82d405e2eb2c75ef189561dd27d5
Russell Snelgrove <22Russell22@gmail.com>
2025-02-09 13:08:16 -0700

    Time: 0 ms (100.00%) | Memory: 50.7 MB (78.89%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+    let startNode = new ListNode(null);
+    startNode.next = head;
+    let counter = 0;
+    let nPointer = startNode;
+    let traversalPointer = startNode;
+    while (traversalPointer !== null) {
+        if (n+1 === counter) {
+            nPointer = nPointer.next;
+        } else {
+            counter++;
+        }
+        traversalPointer = traversalPointer.next;
+    }
+    nPointer.next = nPointer.next?.next ?? null;
+    return startNode.next;
+};
