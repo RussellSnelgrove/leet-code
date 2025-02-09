e4f1535d33929dd6f2a114f4eecd0907cb961553
Russell Snelgrove <22Russell22@gmail.com>
2025-02-08 23:05:55 -0700

    Time: 0 ms (100.00%) | Memory: 51.7 MB (67.62%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -13,20 +13,21 @@
 var mergeTwoLists = function (list1, list2) {
     let currentNode = new ListNode(null);
     const startNode = currentNode;
-    while (list1 && list2){
-        if (list1.val>list2.val){
+    while(list1 !== null && list2 !== null){
+        if (list1.val > list2.val) {
             currentNode.next = list2;
-              list2 = list2.next;
-      } else {
+            list2 = list2.next;
+        } else {
             currentNode.next = list1;
             list1 = list1.next;
         }
         currentNode = currentNode.next;
     }
-    if(list1){
-        currentNode.next = list1;  
-    } else{
-        currentNode.next=list2;
+    if (list1) {
+        currentNode.next = list1;
+    }
+    if (list2) {
+        currentNode.next = list2;
     }
     return startNode.next;
 };
