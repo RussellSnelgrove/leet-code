75024dc34a6b6ac89cb4a0abe4aeaeea6eba6d4e
Russell Snelgrove <22Russell22@gmail.com>
2025-02-12 20:29:56 -0700

    Time: 0 ms (100.00%) | Memory: 49.2 MB (38.15%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var modifiedBinarySearch = function (nums, target, leftPointer, rightPointer, index) {
+    if (leftPointer > rightPointer) return -1;
+    const middlePointer = leftPointer + Math.floor((rightPointer - leftPointer) / 2);
+    const value = nums[middlePointer];
+    const lValue = nums[leftPointer];
+    const rValue = nums[rightPointer];
+    if (value === target) return middlePointer;
+    if (lValue <= value) {
+        if (target > value || target < lValue) return modifiedBinarySearch(nums, target, middlePointer + 1, rightPointer, index);
+        return modifiedBinarySearch(nums, target, leftPointer, middlePointer - 1, index);
+    } else {
+        if (target < value || target > rValue) return modifiedBinarySearch(nums, target, leftPointer, middlePointer - 1, index);
+        return modifiedBinarySearch(nums, target, middlePointer + 1, rightPointer, index);
+    }
+}
+
+var search = function (nums, target) {
+    return modifiedBinarySearch(nums, target, 0, nums.length -1, -1);
+};
