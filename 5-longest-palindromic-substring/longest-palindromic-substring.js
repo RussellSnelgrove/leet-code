79567349fe7e772c55720d0d6cb3886797bd35fd
Russell Snelgrove <22Russell22@gmail.com>
2025-02-14 14:20:15 -0700

    Time: 46 ms (34.98%) | Memory: 51.9 MB (66.59%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+    let string = '';
+    let stringLength = 0;
+    for (let i = 0; i < s.length; i++) {
+        let lPointer = i;
+        let rPointer = i;
+        while (s[lPointer] === s[rPointer] && lPointer >= 0 && rPointer < s.length) {
+            if (rPointer - lPointer + 1 > stringLength) {
+                string = s.substring(lPointer, rPointer + 1);
+                stringLength = rPointer - lPointer + 1;
+            }
+            lPointer--;
+            rPointer++;
+        }
+
+        lPointer = i;
+        rPointer = i + 1;
+        while (s[lPointer] === s[rPointer] && lPointer >= 0 && rPointer < s.length) {
+            if (rPointer - lPointer + 1 > stringLength) {
+                string = s.substring(lPointer, rPointer + 1);
+                stringLength = rPointer - lPointer + 1;
+            }
+            lPointer--;
+            rPointer++;
+        }
+    }
+    return string;
+};
