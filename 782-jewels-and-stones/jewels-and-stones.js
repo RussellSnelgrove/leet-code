698e652d598a47f23ddbc3483b8dcf38ccfbabfb
Russell Snelgrove <22Russell22@gmail.com>
2025-02-14 23:17:14 -0700

    Time: 0 ms (100.00%) | Memory: 48.5 MB (98.86%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -7,8 +7,6 @@ var numJewelsInStones = function (jewels, stones) {
     jewels.split('');
     stones.split('');
     let jewelsCount = 0;
-    for (let i = 0; i < stones.length; i++) {
-        if (jewels.includes(stones[i])) jewelsCount++;
-    }
+    for (let i = 0; i < stones.length; i++) if (jewels.includes(stones[i])) jewelsCount++;
     return jewelsCount;
 };
