7598b59f8677fcb6deb3c0f6bcb26c75fd164202
Russell Snelgrove <22Russell22@gmail.com>
2025-02-18 01:24:52 -0700

    Time: 0 ms (100.00%) | Memory: 54 MB (6.25%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.js
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.js
@@ -3,18 +3,18 @@
  * @return {string}
  */
 var smallestNumber = function (pattern) {
-    let array = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    let result = ''
-    for (let i = 0; i < pattern.length; i++) {
-        if (pattern[i] === 'I') {
-            result += array.shift();
-        } else {
-            let dCount = 1;
-            while(pattern[i+dCount] === 'D') dCount++;
-            result += array[dCount];
-            array.splice(dCount, 1);
+    let result = "";
+    let stack = [];
+
+    for (let i = 0; i <= pattern.length; i++) {
+        stack.push(i + 1);
+
+        if (i === pattern.length || pattern[i] === 'I') {
+            while (stack.length) {
+                result += stack.pop();
+            }
         }
     }
-    result += array.shift();
-    return result
+
+    return result;
 };
