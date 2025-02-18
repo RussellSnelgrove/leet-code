9c13b4fb6f90fa71beecace705db9bb09bcfcbe2
Russell Snelgrove <22Russell22@gmail.com>
2025-02-17 23:39:40 -0700

    Time: 4 ms (60.04%) | Memory: 53.4 MB (16.40%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,12 +3,14 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-    const numsSet = {}
-    let majorityElement;
-    for (let i = 0; i < nums.length; i++) {
-        numsSet[nums[i]] ? numsSet[nums[i]]++ : numsSet[nums[i]] = 1;
-        if(majorityElement === undefined) majorityElement = nums[i]
-        if (numsSet[majorityElement] && numsSet[nums[i]] > numsSet[majorityElement]) majorityElement = nums[i];
-    }
-    return majorityElement
+    // const numsSet = {}
+    // let majorityElement;
+    // for (let i = 0; i < nums.length; i++) {
+    //     numsSet[nums[i]] ? numsSet[nums[i]]++ : numsSet[nums[i]] = 1;
+    //     if(majorityElement === undefined) majorityElement = nums[i]
+    //     if (numsSet[majorityElement] && numsSet[nums[i]] > numsSet[majorityElement]) majorityElement = nums[i];
+    // }
+    // return majorityElement
+    nums.sort((a,b) => {return a-b});
+    return nums[Math.floor(nums.length/2)]
 };
