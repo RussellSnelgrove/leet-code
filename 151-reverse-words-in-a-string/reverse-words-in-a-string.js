be2cdecdc04af0f14fbc1e71e28a0b162090a5c8
Russell Snelgrove <22Russell22@gmail.com>
2025-02-19 11:50:46 -0700

    Time: 0 ms (100.00%) | Memory: 53.8 MB (5.11%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -3,15 +3,5 @@
  * @return {string}
  */
 var reverseWords = function (s) {
-    const sArray = s.trim().split(' ').reverse();
-    let i = 0;
-    while (i < sArray.length) {
-        sArray[i] = sArray[i].trim();
-        if (sArray[i] === '') {
-            sArray.splice(i, 1);
-        } else {
-            i++;
-        }
-    }
-    return sArray.join(' ');
+        return s.trim().split(/\s+/).reverse().join(' ');
 };
