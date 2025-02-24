eddb2460723ce17166997fb938c3dbf97065dbb7
Russell Snelgrove <22Russell22@gmail.com>
2025-02-23 22:25:12 -0700

    Time: 6 ms (40.37%) | Memory: 57.2 MB (6.23%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,14 +3,5 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-    // const numsSet = {}
-    // let majorityElement;
-    // for (let i = 0; i < nums.length; i++) {
-    //     numsSet[nums[i]] ? numsSet[nums[i]]++ : numsSet[nums[i]] = 1;
-    //     if(majorityElement === undefined) majorityElement = nums[i]
-    //     if (numsSet[majorityElement] && numsSet[nums[i]] > numsSet[majorityElement]) majorityElement = nums[i];
-    // }
-    // return majorityElement
-    nums.sort((a,b) => {return a-b});
-    return nums[Math.floor(nums.length/2)]
+    return nums.sort()[Math.floor(nums.length/2)]
 };
