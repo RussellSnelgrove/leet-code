e6697201176e0bb5d155a3a60f39fd9b5124a5c2
Russell Snelgrove <22Russell22@gmail.com>
2025-02-23 22:48:38 -0700

    Time: 0 ms (100.00%) | Memory: 64.2 MB (8.58%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,21 +4,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-    // two options
-    // option1 loop through and pop then unshift
-    // option2 splice last k elements then concat first half to second
-    // option 1 uses less memory but runs longer
     if (k > nums.length) {
         for (let i = 0; i < k; i++) {
-            nums.unshift(nums.pop())
+            const element = nums.pop();
+            nums.unshift(element);
         }
     } else {
-        const array = nums.splice(nums.length - k, k)
+        const array = nums.splice(nums.length - k, k);
         nums.unshift(...array)
-    }
 
-    // excceed run time
-    // for (let i = 0; i < k; i++) {
-    //     nums.unshift(nums.pop())
-    // }
+    }
 };
