2a16081107ac8ed0b7d117a8d9972f9c1bfbbc33
Russell Snelgrove <22Russell22@gmail.com>
2025-02-25 17:14:35 -0700

    Time: 1 ms (92.68%) | Memory: 58.9 MB (5.62%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function (words, x) {
+    const containsArray = [];
+    for (let i = 0; i < words.length; i++) {
+        if(words[i].includes(x)) containsArray.push(i)
+    }
+    return containsArray;
+};
