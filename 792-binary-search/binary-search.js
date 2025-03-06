0be6ed00759749691edfa7a2decfa1d53fe66b89
Russell Snelgrove <22Russell22@gmail.com>
2025-03-06 12:25:04 -0700

    Time: 0 ms (100.00%) | Memory: 57.5 MB (20.60%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -3,6 +3,17 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    return nums.indexOf(target);
+
+var binarySearch = function (nums, target, left, right) {
+    const midIndex = left + Math.floor((right - left) / 2);
+    if (nums[midIndex] === target) return midIndex;
+    if(left >= right) return -1;
+    if (nums[midIndex] > target) return binarySearch(nums, target, left, midIndex - 1);
+    if (nums[midIndex] < target) return binarySearch(nums, target, midIndex + 1, right);
+    // if(left === right )
+}
+var search = function (nums, target) {
+    return binarySearch(nums, target, 0, nums.length-1);
+    // Cheat way
+    // return nums.indexOf(target);
 };
