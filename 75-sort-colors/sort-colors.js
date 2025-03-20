f3c9cffe7e10beb1fb1119ab15269650d370cb40
Russell Snelgrove <22Russell22@gmail.com>
2025-03-20 15:41:49 -0600

    Time: 1 ms (32.66%) | Memory: 53.1 MB (44.59%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,22 +3,30 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    let elementsSorted = 0;
-    let index = 0;
-    while (elementsSorted < nums.length) {
-        if (nums[index] === 0) {
-            nums.unshift(...nums.splice(index, 1))
-            index++;
-            // go to next element since behind is sorted
-        } else if (nums[index] === 1){
-            index++
-            // go to next element since elements behind are sorted
-        } else {
-            nums.push(...nums.splice(index, 1))
-            // dont go to next element since element in positioon is not sorted
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] < nums[i]) {
+                const temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+            }
         }
-        elementsSorted++;
     }
-    console.log(nums)
-    return nums;
+    // let elementsSorted = 0;
+    // let index = 0;
+    // while (elementsSorted < nums.length) {
+    //     if (nums[index] === 0) {
+    //         nums.unshift(...nums.splice(index, 1))
+    //         index++;
+    //         // go to next element since behind is sorted
+    //     } else if (nums[index] === 1){
+    //         index++
+    //         // go to next element since elements behind are sorted
+    //     } else {
+    //         nums.push(...nums.splice(index, 1))
+    //         // dont go to next element since element in positioon is not sorted
+    //     }
+    //     elementsSorted++;
+    // }
+    // return nums;
 };
