d4ee23fe3df95ae9e3ba1a733c5bc12bef5ac460
Russell Snelgrove <22Russell22@gmail.com>
2025-03-29 12:17:17 -0600

    Time: 4 ms (87.24%) | Memory: 59.9 MB (34.06%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.js
+++ 6-zigzag-conversion/zigzag-conversion.js
@@ -4,22 +4,34 @@
  * @return {string}
  */
 var convert = function (s, numRows) {
-    const resultArray = new Array(numRows).fill('');
-    const sArray = s.split('');
-    while (sArray.length !== 0) {
-        // resultArray.forEach((row) => {
-        //     row += sArray.pop();
-        // })
-        for (let i = 0; i < numRows; i++) {
-            if (sArray.length === 0) break;
-            resultArray[i] += sArray.shift();
-        }
-        if (sArray.length === 0) break;
-        for (let i = numRows - 2; i > 0; i--) {
-            if (sArray.length === 0) break;
-            resultArray[i] += sArray.shift();
+    if (numRows === 1) return s;
+    let result = '';
+    for (let row = 0; row < numRows; row++) {
+        const increment = 2 * (numRows - 1);
+        for (let i = row; i < s.length; i += increment) {
+            result += s[i];
+            if (row > 0 && row < numRows - 1 && (i + increment - 2 * row) < s.length) {
+                result += s[i + increment - 2 * row];
+            }
         }
     }
-    console.log('resultArray', resultArray);
-    return resultArray.join('');
+    return result;
+    // const resultArray = new Array(numRows).fill('');
+    // const sArray = s.split('');
+    // while (sArray.length !== 0) {
+    //     // resultArray.forEach((row) => {
+    //     //     row += sArray.pop();
+    //     // })
+    //     for (let i = 0; i < numRows; i++) {
+    //         if (sArray.length === 0) break;
+    //         resultArray[i] += sArray.shift();
+    //     }
+    //     if (sArray.length === 0) break;
+    //     for (let i = numRows - 2; i > 0; i--) {
+    //         if (sArray.length === 0) break;
+    //         resultArray[i] += sArray.shift();
+    //     }
+    // }
+    // console.log('resultArray', resultArray);
+    // return resultArray.join('');
 };
