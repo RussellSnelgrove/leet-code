5630a18355588c3595377cb07a0463792ef2d9a5
Russell Snelgrove <22Russell22@gmail.com>
2025-04-03 17:11:07 -0600

    Time: 15 ms (47.56%) | Memory: 56.9 MB (75.54%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,30 +4,19 @@
  * @return {number[]}
  */
 var topKFrequent = function (nums, k) {
-    // const res = [];
-    // const hashNumFrequency = {};
-    // for (const num in nums){
-    //     hashNumFrequency[nums[num]] = (hashNumFrequency[nums[num]] || 0) + 1;
-    // }
-    // const bucketOfFrequency = Array.from({ length: nums.length + 1 }, () => []);
-
-    // for ( const num in hashNumFrequency){
-    //   bucketOfFrequency[hashNumFrequency[num]].push(parseInt(num));
-    // }
-    // for(let i = bucketOfFrequency.length-1; i > 0; i--){
-    //   for (const n of bucketOfFrequency[i]){
-    //     res.push(n);
-    //     if(res.length ===k) return res;
-    //   }
-    // }
-
-    const result = {};
-  
+    // map an object with number and count
+    // sort it by frequency
+    const numsMap = {};
     for (let i = 0; i < nums.length; i++) {
-        if (result[nums[i]] === undefined) result[nums[i]] = 0;
-        result[nums[i]]++;
+        const num = nums[i];
+        if (numsMap[num] === undefined) numsMap[num] = 0;
+        numsMap[num] += 1;
     }
-    const arr = Object.entries(result).map(([num, freq]) => [freq, parseInt(num)]);
-    arr.sort((element1, element2) => element2[0] - element1[0]);
-    return arr.slice(0,k).map(element => element[1])
+    var array = Object.keys(numsMap).map((key) => [key, numsMap[key]]);
+    array.sort((a, b) => a[1] - b[1]);
+    const result = [];
+    for(let i = 0; i<k; i++){
+        result.push(parseInt(array.pop()[0]));
+    }
+    return result;
 };
