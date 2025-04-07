51c07c0216c5a47bb38edc35dc43ddb5730b2484
Russell Snelgrove <22Russell22@gmail.com>
2025-04-07 14:58:22 -0600

    Time: 55 ms (65.38%) | Memory: 58.3 MB (14.30%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.ts
@@ -0,0 +1,7 @@
+function reverse(x: number): number {
+    let string1: string = x.toString();
+    string1 = string1.split('').reverse().join('');
+    const result: number = parseInt(string1);
+    if (result > 2147483647 || result < -2147483648) return 0;
+    return x < 0 ? result * -1 : result;
+};
