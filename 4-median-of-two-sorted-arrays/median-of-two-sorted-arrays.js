4afdaf960ca8982d34d94c550325b35f9a1d70b4
Russell Snelgrove <22Russell22@gmail.com>
2025-05-02 09:25:43 -0600

    Time: 2 ms (89.00%) | Memory: 60.4 MB (20.08%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -5,12 +5,9 @@
  */
 var findMedianSortedArrays = function (nums1, nums2) {
     const newArray = nums1.concat(nums2).sort((a, b) => a - b);
-    console.log(newArray);
     if (newArray.length % 2 === 1) {
-        console.log(newArray);
         return (newArray[Math.floor(newArray.length / 2)]);
     } else {
-        console.log(newArray);
         return (newArray[newArray.length / 2] + newArray[newArray.length / 2 - 1]) / 2;
     }
 };
