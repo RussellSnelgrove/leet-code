2cedf3e601f669d8db41e3d408f27c41950ae717
Russell Snelgrove <22Russell22@gmail.com>
2025-05-10 23:37:20 -0600

    Time: 0 ms (100.00%) | Memory: 55.2 MB (19.55%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.js
+++ 1293-three-consecutive-odds/three-consecutive-odds.js
@@ -3,14 +3,10 @@
  * @return {boolean}
  */
 var threeConsecutiveOdds = function (arr) {
-    let count = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 === 1) {
-            count++;
-            if (count === 3) return true;
-        } else {
-            count = 0;
-        }
+    for (let i = 0; i < arr.length - 2; i++) {
+        if (arr[i] % 2 === 1
+            && arr[i + 1] % 2 === 1
+            && arr[i + 2] % 2 === 1) return true;
     }
     return false;
 };
