e9f86a86fd55cd0426466f4ed86d361f71c516de
Russell Snelgrove <22Russell22@gmail.com>
2025-11-18 17:27:03 -0700

    Time: 51 ms (32.84%) | Memory: 218.1 MB (10.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.rb
+++ 242-valid-anagram/valid-anagram.rb
@@ -4,15 +4,19 @@
 def is_anagram(s, t)
     # option 1 is to sort both and compare 2log(n) * 2n
     # option 2 is to make a hash and compare the hashes 2n + 26
-    return false if s.length != t.length
-    sMap = Hash.new(0)
-    tMap = Hash.new(0)
-    s.each_char.with_index do |char, index|
-        sMap[s[index]]+=1
-        tMap[t[index]]+=1
-    end
-    sMap.each do |key, value|
-        return false if tMap[key] != value
-    end
-    return true
+    return s.chars.sort.join === t.chars.sort.join
+
+
+
+    # return false if s.length != t.length
+    # sMap = Hash.new(0)
+    # tMap = Hash.new(0)
+    # s.each_char.with_index do |char, index|
+    #     sMap[s[index]]+=1
+    #     tMap[t[index]]+=1
+    # end
+    # sMap.each do |key, value|
+    #     return false if tMap[key] != value
+    # end
+    # return true
 end
