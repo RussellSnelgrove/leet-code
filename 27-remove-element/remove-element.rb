26c47679071c3dc7c09b00e2fd12f7d2b4acb7fb
Russell Snelgrove <22Russell22@gmail.com>
2025-11-22 22:32:42 -0700

    Time: 0 ms (100.00%) | Memory: 211.4 MB (97.84%) - LeetSync

--- 27-remove-element/remove-element.rb
+++ 27-remove-element/remove-element.rb
@@ -2,6 +2,21 @@
 # @param {Integer} val
 # @return {Integer}
 def remove_element(nums, val)
-    puts nums.delete(val)
+    # nums.delete(val)
+    # return nums.length()
+
+    index = 0;
+    index_count = 0;
+    nums_length = nums.length()
+    while index_count < nums_length
+        if nums[index] == val
+            nums.delete_at(index)
+            index_count += 1
+        else
+            index += 1
+            index_count +=1
+
+        end
+    end
     return nums.length()
 end
