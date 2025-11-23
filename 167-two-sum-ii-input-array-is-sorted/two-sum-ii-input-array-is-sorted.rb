c9ca17f4c30c00c5d7f42f41834464906d316a8f
Russell Snelgrove <22Russell22@gmail.com>
2025-11-23 11:17:50 -0700

    Time: 3 ms (87.04%) | Memory: 212.5 MB (25.93%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rb
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.rb
@@ -5,11 +5,12 @@ def two_sum(numbers, target)
     left_pointer_index = 0;
     right_pointer_index = numbers.length()-1;
     while left_pointer_index < right_pointer_index
-        return [left_pointer_index+1, right_pointer_index+1] if (numbers[left_pointer_index] + numbers[right_pointer_index] == target)
         if numbers[left_pointer_index] + numbers[right_pointer_index] > target
             right_pointer_index -= 1
-        else
+        elsif numbers[left_pointer_index] + numbers[right_pointer_index] < target
             left_pointer_index += 1
+        else
+            return [left_pointer_index+1, right_pointer_index+1]   
         end
     end
 end
