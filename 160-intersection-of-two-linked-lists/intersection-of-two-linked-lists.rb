98943cafe51843e7ccbc780af91dc97a77d07cba
Russell Snelgrove <22Russell22@gmail.com>
2025-12-13 00:48:08 -0700

    Time: 140 ms (9.68%) | Memory: 216.8 MB (35.48%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.rb
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.rb
@@ -11,16 +11,24 @@
 # @param {ListNode} headB
 # @return {ListNode}
 def getIntersectionNode(headA, headB)
-    a_set = Set.new
-    current_node = headA
-    while current_node != nil
-        a_set.add(current_node)
-        current_node = current_node.next
+    # a_set = Set.new
+    # current_node = headA
+    # while current_node != nil
+    #     a_set.add(current_node)
+    #     current_node = current_node.next
+    # end
+    # current_node = headB
+    # while current_node != nil
+    #     return current_node if a_set.include?(current_node)
+    #     current_node = current_node.next
+    # end
+    # return nil
+    temp_a = headA
+    temp_b = headB
+    while temp_a != temp_b
+        temp_a = temp_a.nil? ? headB : temp_a.next
+        temp_b = temp_b.nil? ? headA : temp_b.next
     end
-    current_node = headB
-    while current_node != nil
-        return current_node if a_set.include?(current_node)
-        current_node = current_node.next
-    end
-    return nil
+
+    return temp_a
 end
