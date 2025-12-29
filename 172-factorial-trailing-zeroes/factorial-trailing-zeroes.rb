9b4ecafe714c8dd8d5e94227e8bdea3c7c530660
Russell Snelgrove <22Russell22@gmail.com>
2025-12-29 12:24:11 -0700

    Time: 0 ms (100.00%) | Memory: 211.5 MB (14.29%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.rb
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.rb
@@ -1,10 +1,8 @@
-# @param {Integer} n
-# @return {Integer}
 def trailing_zeroes(n)
-  k = Math.log(n, 5)
   count = 0
-  (1..k).each do |i|
-    count += n / (5**i)
+  while n > 0
+    n /= 5
+    count += n
   end
   count
 end
