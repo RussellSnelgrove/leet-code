546a1fb140424590ef8ab5f9026f44ece2693d65
Russell Snelgrove <22Russell22@gmail.com>
2026-01-13 21:16:20 -0700

    Time: 11 ms (11.11%) | Memory: 211.6 MB (33.33%) - LeetSync

--- 682-baseball-game/baseball-game.rb
+++ 682-baseball-game/baseball-game.rb
@@ -4,16 +4,28 @@ def cal_points(operations)
     result = []
     total = 0
     operations.each do |operation|
-        value = 0 
-        if operation == 'C'
-            value -= result.pop() 
-        else
-            value = (result[-1].to_i + result[-2].to_i) if operation == '+'
-            value = (result[-1].to_i * 2)  if operation == 'D'
-            value = (operation.to_i)  if operation.match(/[0-9]+/)
-            result.push(value)
-        end
+        value = 0
+        case operation
+            when 'C'
+                value -= result.pop() 
+        puts 'C'
+            else
+        puts 'other'
+                value = (result[-1].to_i + result[-2].to_i) if operation == '+'
+                value = (result[-1].to_i * 2)  if operation == 'D'
+                value = (operation.to_i)  if operation.match(/[0-9]+/)
+                result.push(value)
+        end 
         total += value
+
+        # if operation == 'C'
+        #     value -= result.pop() 
+        # else
+        #     value = (result[-1].to_i + result[-2].to_i) if operation == '+'
+        #     value = (result[-1].to_i * 2)  if operation == 'D'
+        #     value = (operation.to_i)  if operation.match(/[0-9]+/)
+        #     result.push(value)
+        # end
     end
     total
 end
