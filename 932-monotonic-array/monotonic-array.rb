02dae113a47ad16284d7bb6b4633041a1b964743
Russell Snelgrove <22Russell22@gmail.com>
2026-01-15 20:45:11 -0700

    Time: 44 ms (75.00%) | Memory: 236.7 MB (25.00%) - LeetSync

--- 932-monotonic-array/monotonic-array.rb
+++ 932-monotonic-array/monotonic-array.rb
@@ -1,14 +1,16 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def is_monotonic(nums)
-    increase = false
-    decrease = false
-    last_num = nums.shift
-    nums.each do | num |
-        increase = true if num > last_num
-        decrease = true if num < last_num
-        return false if increase && decrease
-        last_num = num
-    end
-    true
+    sorted = nums.sort
+    sorted == nums || sorted.reverse == nums
+    # increase = false
+    # decrease = false
+    # last_num = nums.shift
+    # nums.each do | num |
+    #     increase = true if num > last_num
+    #     decrease = true if num < last_num
+    #     return false if increase && decrease
+    #     last_num = num
+    # end
+    
 end
