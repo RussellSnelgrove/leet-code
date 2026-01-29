6f9734e4012ce4a90509833ab14a3a2757c0fe76
Russell Snelgrove <22Russell22@gmail.com>
2026-01-28 20:18:35 -0700

    Time: 48 ms (38.65%) | Memory: 221.1 MB (19.50%) - LeetSync

--- 49-group-anagrams/group-anagrams.rb
+++ 49-group-anagrams/group-anagrams.rb
@@ -7,6 +7,5 @@ def group_anagrams(strs)
         strs_hash[sorted_str] = [] if strs_hash[sorted_str] == nil
         strs_hash[sorted_str].push(str)
     end
-    strs_array = strs_hash.values
-    return strs_array
+    strs_hash.values
 end
