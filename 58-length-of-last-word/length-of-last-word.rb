8cddb50555f170e2bcd60360ebbcb5104e799286
Russell Snelgrove <22Russell22@gmail.com>
2026-01-29 22:53:55 -0700

    Time: 4 ms (2.26%) | Memory: 211.4 MB (67.67%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.rb
+++ 58-length-of-last-word/length-of-last-word.rb
@@ -1,5 +1,12 @@
 # @param {String} s
 # @return {Integer}
 def length_of_last_word(s)
-    s.split(' ').at(-1).length
+    count = 0
+    last_var_a_char = false
+    s.reverse.each_char do |char |
+        return count if char == ' ' && last_var_a_char
+        last_var_a_char = true if char != ' '
+        count += 1 if last_var_a_char
+    end
+    count
 end
